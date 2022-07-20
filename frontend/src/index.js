8d8ca513d37514ed902111dc3d3f3dc1e31e16f1
김성빈 <ksb5535@naver.com>
2022-07-20 15:41:25 +0900

    [UPDATE] 회원가입 폼 수정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Community from './domain/community/communityList'
-import CommunityDetail from './domain/community/communityDetail';
-import CommunityMain from './domain/community/communityMain';
-import PerfumeList from './domain/perfume/perfumeList';
-import PerfumeDetail from './domain/perfume/perfumeDetail';
-import PerfumeMain from './domain/perfume/perfumeMain';
-import Login from './domain/user/Login';
-import PerfumeList from './domain/perfume/perfumeList'
-import PerfumeDetail from './domain/perfume/perfumeDetail'
-import PerfumeRegistList from './domain/perfume/perfumeRegistList';
-import PerfumeRegist from './domain/perfume/perfumeRegist';
-import Login from './domain/user/Login'
-import reportWebVitals from './reportWebVitals';
-import Nav from './components/nav';
-import Footer from './components/footer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Community from "./domain/community/communityList";
+import CommunityDetail from "./domain/community/communityDetail";
+import CommunityMain from "./domain/community/communityMain";
+import PerfumeDetail from "./domain/perfume/perfumeDetail";
+import PerfumeMain from "./domain/perfume/perfumeMain";
+import Login from "./domain/user/Login";
+import PerfumeList from "./domain/perfume/perfumeList";
+import PerfumeRegistList from "./domain/perfume/perfumeRegistList";
+import PerfumeRegist from "./domain/perfume/perfumeRegist";
+import reportWebVitals from "./reportWebVitals";
+import Nav from "./components/nav";
+import Footer from "./components/footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PerfumeRegistList />
-  </React.StrictMode>
+    <PerfumeMain />
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
