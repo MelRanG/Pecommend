cc04c3d4bf0856be7e11e47917e4a43030bf1fb2
박유주 <qkrdbwn98@naver.com>
2022-07-25 14:58:56 +0900

    [UPDATE] home / pont

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,19 +15,22 @@ import Profile from "./domain/user/profile";
 import reportWebVitals from "./reportWebVitals";
 import Nav from "./components/nav";
 import Footer from "./components/footer";
+import Home from "./domain/home/home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-{/* <link
+{
+  /* <link
   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
   rel="stylesheet"
   integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
   crossorigin="anonymous"
-></link>; */}
+></link>; */
+}
 
 root.render(
   <React.StrictMode>
-    <PerfumeMain />
-  </React.StrictMode>,
+    <Home />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
