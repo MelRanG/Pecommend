4d4a5a37c8d5d70e062ad51a5e97d164c2cad605
ParkYuJu <qkrdbwn98@naver.com>
2022-07-26 21:11:05 +0900

    [UPDATE] home font 회의 이후

--- frontend/src/domain/home/home.js
+++ frontend/src/domain/home/home.js
@@ -11,7 +11,7 @@ const home = () => {
         <div className="container">
           <div className="row home-parent">
             <div className="home-title">
-              <p>LikeDisLike</p>
+              <p style={{ display: "inline" }}>LIKE</p><p style={{ color: "rgb(92 100 104)", display: "inline" }}>DISLIKE</p>
               <h2>We bring you closer to your goals</h2>
             </div>
           </div>
@@ -33,14 +33,14 @@ const home = () => {
             </div>
             <div className="col feature-box">
               <img src=".\assets\img/home/feature2.jpg" alt="" />
-              <h3>취향</h3>
+              <h3>추천</h3>
               <p>
                 선호 / 비선호 정보로
               </p>
             </div>
             <div className="col feature-box">
               <img src=".\assets\img/home/feature3.jpg" alt="" />
-              <h3>향수</h3>
+              <h3>취향</h3>
               <p>
                 나에게 맞는 향수를
               </p>
@@ -60,9 +60,14 @@ const home = () => {
                   인기향수와 선호도에 따른 다양한 향수를 만나보세요!<br />
                   향수 기본 정보와 선호 비율, 특징있는 해시태그, 리뷰 그리고 선호 / 비선호에 따른 연관 향수 추천까지!
                 </div>
-                <button type="button" class="btn btn-outline-light">
+                {/* <button type="button" class="btn btn-outline-light">
                   &nbsp;바로가기&nbsp;
-                </button>
+                </button> */}
+                <div className="home-product-hashtag">
+                  <div className="" style={{ backgroundColor: "rgb(241 169 131)" }}>#좋아요</div>
+                  <div className="" style={{ backgroundColor: "rgb(199 188 187)" }}>#싫어요</div>
+                  <div className="" style={{ backgroundColor: "rgb(171 145 146)" }}>#리뷰</div>
+                </div>
               </div>
             </div>
           </div>
@@ -74,15 +79,25 @@ const home = () => {
                   향수, 질문, 자유 등의 다양한 게시판을 통해 카테고리 별로 다른 유저들과 의견을 나눠보세요! <br />
                   인기 게시판과 베스트 게시판을 통해 유저들의 관심 게시글을 확인해보세요!
                 </div>
+                <div className="home-product-hashtag">
+                  <div className="" style={{ backgroundColor: "rgb(153 187 255)" }}>#질문</div>
+                  <div className="" style={{ backgroundColor: "#deb6ff" }}>#자유</div>
+                  <div className="" style={{ backgroundColor: "#ffda3c" }}>#인기</div>
+                </div>
               </div>
             </div>
           </div>
           <div className="home-banner3">
             <div className="banner-box">
               <div className="banner-total">
-                <div className="banner-title">TEST</div>
+                <div className="banner-title">to be continue</div>
                 <div className="banner-text">
-                  향수를 처음 접하신다면? 간단한 Test를 통해서 향수를 추천받으며 향수를 접해보고 알아가보세요!
+                  {/* 향수를 처음 접하신다면? 간단한 Test를 통해서 향수를 추천받으며 향수를 접해보고 알아가보세요! */}
+                  --준비중--
+                </div>
+                <div className="home-product-hashtag">
+                  <div className="" style={{ backgroundColor: "rgb(120 126 114)" }}>#TEST</div>
+                  <div className="" style={{ backgroundColor: "rgb(201 177 155)" }}>#MBTI</div>
                 </div>
               </div>
             </div>
@@ -90,7 +105,7 @@ const home = () => {
         </div>
       </div>
 
-      <div className="container">asdf</div>
+
 
       <Footer />
     </div>
