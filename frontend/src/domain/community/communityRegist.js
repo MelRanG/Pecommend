d2927612602d9027118089945d6fc550a90de051
박지하 <bgpjh97@gmail.com>
2022-07-26 13:59:03 +0900

    [UPDATE] 커뮤니티 게시글 작성 기능 추가

--- frontend/src/domain/community/communityRegist.js
+++ frontend/src/domain/community/communityRegist.js
@@ -1,7 +1,45 @@
 import './communityRegist.css'
 import Nav from "../../components/nav";
+import axios from 'axios';
+import React, {useState} from 'react';
 
-function communityRegist () {
+function CommunityRegist ()  {
+    const [formValue, setForm] = useState({
+        username: '',
+        title: '',
+        content: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({
+            ...formValue,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        let registwrite = new FormData();
+        registwrite.append('name', formValue.name)
+        registwrite.append('content',formValue.content)
+        console.log(registwrite)
+        try {
+            const response = await axios({
+                method: "post",
+                url: "/api/v1/community",
+                data: registwrite,
+                headers: { "Content-Type": "multipart/form-data"},
+                // JSON.stringify()
+            });
+            if(response.status == 201) {
+                console.log('!!!')
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="communityRegist">
             <Nav />
@@ -14,21 +52,21 @@ function communityRegist () {
                         <span>카테고리</span>
                     </div>
                     <div className="regist-topbar-item-context">
-                        <div class="dropdown">
-                            <button class="regist-dropdown dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div className="dropdown">
+                            <button className="regist-dropdown dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown button
                             </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                <li><a className="" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li>
+                                <li><a className="dropdown-item" href="#">Something else here</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <hr className="hrtag"></hr>
                 <div className="regist-topbar-item">
-                    <div className="regist-topbar-item-name">
+                    <div className="regist-topbar-item-name" onChange={handleChange}>
                         <span>제목</span>
                     </div>
                     <div className="regist-topbar-item-context context-title">
@@ -46,13 +84,13 @@ function communityRegist () {
                 </div>
                 <hr className='hrtag'></hr>
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
             <div className='community-regist-text'>
-                <textarea className="regist-textarea" rows="15"></textarea>
+                <textarea className="regist-textarea" rows="15" onChange={ handleChange }></textarea>
             </div>
 
             <div className='community-regist-bottombar'>
-                <button className="regist-submit" type='submit'>
+                <button className="regist-submit">
                     등록하기
                 </button>
                 <button className='regist-cancel'>
@@ -64,4 +102,4 @@ function communityRegist () {
     );
 }
 
-export default communityRegist;
+export default CommunityRegist;
