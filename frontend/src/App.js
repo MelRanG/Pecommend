0245e9edce35fa7e165d3b37ccc548f84e4afaeb
안세영 <tpdud285@naver.com>
2022-07-28 16:21:00 +0900

    [UPDATE] 머지 후 잔여물

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CommunityList from "./domain/community/communityList";
 import CommunityDetail from "./domain/community/communityDetail";
 import CommunityMain from "./domain/community/communityMain";
 import CommunityRegist from "./domain/community/communityRegist";
+import Perfume from "./domain/perfume/perfume";
 import PerfumeList from "./domain/perfume/perfumeList";
 import PerfumeDetail from "./domain/perfume/perfumeDetail";
 import PerfumeMain from "./domain/perfume/perfumeMain";
