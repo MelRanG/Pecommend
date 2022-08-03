0ed9426aaa62917ea891ce4a5f599f52441dccdb
박주연 <18cwceark@gmail.com>
2022-08-03 15:13:31 +0900

    [ADD] perfumeRequestDto 추가

---
+++ backend/src/main/java/com/perfume/perfumeservice/dto/perfume/PerfumeRequestDto.java
@@ -0,0 +1,28 @@
+package com.perfume.perfumeservice.dto.perfume;
+
+import com.perfume.perfumeservice.domain.perfume.Perfume;
+
+public class PerfumeRequestDto {
+
+    //private long perfumeId;
+    private String koName;
+    private String enName;
+    // private Double score; // 평점
+    private int perfumeLike;
+    private int perfumeDisLike;
+    private String density;
+
+    public Perfume toEntity(Perfume perfume){
+        return Perfume.builder()
+                .koName(koName)
+                .enName(enName)
+                .perfumeLike(perfumeLike)
+                .perfumeDislike(perfumeDisLike)
+                .density(density)
+                .build();
+
+    }
+    // 삽입
+
+
+}
