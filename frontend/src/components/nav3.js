ab61d7139e8a138f044af7c175b8c207fed5cae4
박유주 <qkrdbwn98@naver.com>
2022-08-04 17:36:31 +0900

    [UPDATE] 연관향수추천

--- frontend/src/components/nav3.js
+++ frontend/src/components/nav3.js
@@ -2,30 +2,27 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import React from "react";
 import "./nav3.css";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 
 function Nav3() {
-
-  const isLogined = useSelector(state=>state.userStore.isLogined)
+  const isLogined = useSelector((state) => state.userStore.isLogined);
 
   useEffect(() => {
-    const toggleBtn = document.querySelector('.navbar__toogleBtn');
-    const menu = document.querySelector('.navbar__menu');
-    const icons = document.querySelector('.navbar__icons');
+    const toggleBtn = document.querySelector(".navbar__toogleBtn");
+    const menu = document.querySelector(".navbar__menu");
+    const icons = document.querySelector(".navbar__icons");
 
-    toggleBtn.addEventListener('click', () => {
-      menu.classList.toggle('active');
-      icons.classList.toggle('active');
+    toggleBtn.addEventListener("click", () => {
+      menu.classList.toggle("active");
+      icons.classList.toggle("active");
     });
-  })
-
+  });
 
   return (
     <nav className="navbar">
       <div className="navbar__logo">
         <Link to={"/"}>
           <img
-
             alt="?"
             src="assets/img/logo/logo-3.PNG"
             style={{ height: "60px", marginBottom: "5px", marginTop: "5px" }}
@@ -43,22 +40,25 @@ function Nav3() {
           <Link to="/test">TEST</Link>
         </li>
       </ul>
-      {isLogined ? 
-      <ul className="navbar__icons">
-        <li>
-          <Link to="/myprofile">MY PROFILE</Link>
-        </li>
-        <li>
-          <Link to="/logout">LOGOUT</Link>
-        </li>
-      </ul> : 
-      <ul className="navbar__icons">
-        <li>
-          <Link to="/login">LOGIN&nbsp;/&nbsp;REGIST</Link>
-        </li>
-      </ul>
-      }
-      <a href="#" className="navbar__toogleBtn"><i className="fa-solid fa-bars"></i></a>
+      {isLogined ? (
+        <ul className="navbar__icons">
+          <li>
+            <Link to="/myprofile">MY PROFILE</Link>
+          </li>
+          <li>
+            <Link to="/logout">LOGOUT</Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="navbar__icons">
+          <li>
+            <Link to="/login">LOGIN&nbsp;/&nbsp;REGIST</Link>
+          </li>
+        </ul>
+      )}
+      <a href="#" className="navbar__toogleBtn">
+        <i className="fa-solid fa-bars"></i>
+      </a>
     </nav>
   );
 }
